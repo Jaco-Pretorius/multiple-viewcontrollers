3aa04104be143cfa7c8e8c59bc85f12e07c80c2f
Jaco Pretorius <jaco.pretorius@thoughtworks.com>
2013-04-19 09:09:29 -0400

    Initial Commit

---
+++ MultipleViewControllers/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MultipleViewControllers
+//
+//  Created by Jaco on 4/19/13.
+//  Copyright (c) 2013 Jaco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MultipleViewControllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MultipleViewControllers
+//
+//  Created by Jaco on 4/19/13.
+//  Copyright (c) 2013 Jaco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
